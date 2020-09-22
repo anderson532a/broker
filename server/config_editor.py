b4420271680e87843640d342c6fb43943b04fbe0
compal_andersonCJ <andersonCJ_Chen@compal.com>
2020-09-22 13:27:21 +0800

    9/22

--- server/config_editor.py
+++ server/config_editor.py
@@ -1,88 +1,72 @@
 import os
-import SQL_connect
 import logging
 from pathlib import Path
-import shutil
+FORMAT = "%(asctime)s %(levelname)s:%(message)s"
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 
 c = ".config"
-
 configpath = "C:\\gaminganywhere-0.8.0\\bin\\config\\"
 
-'''
-class read_config:
-    def __init__(self, name):
-        os.chdir(configpath)
-        self.name = name
-        f = open(self.name,'r')
-    def read(self,num = None):
-        self.num = num
-        if nself.num != None:
-            conf = f.read(self.num)
-        else:
-            conf = f.readlines()
-        return conf
-    def close(self):
-        f.close()
-'''
-class find_match:
+class read_in:
 # read config file  
-    def __init__(self, name):
-        os.chdir(configpath)
+    def __init__(self, name, dir):
+        if dir == 'C':
+            os.chdir(configpath)
+            logging.debug(f"{Path.cwd()}")
+        else:
+            logging.debug(f"{Path.cwd()}")
         self.name = name
-        self.f = open(self.name,'r')
+        try:
+            with open(self.name,'r') as fr:
+                self.list = fr.readlines()
+                logging.debug(f"{self.list}")
 
-    def scan_line(self, word):
-        self.word = word
-        line = 0
-        for i in self.f:
-            line +=1
-            if i == self.word:
-                return line    
-        return 0
+        except FileNotFoundError:
+            logging.error("can't find", exc_info=True)
 
-    def scan_head(self, word):
-        self.word = word
-        line = 0
-        for i in self.f:
-            line +=1
-            A = i.split("=",1)
-            B = self.word.split("=",1)
-            if A[0].rstrip() == B[0].rstrip():
-                return line
-            else:
-                return 0
-    def close(self):
-        self.f.close()
 
-# write config data to local
-class edit_config(find_match):
-    def __init__(self, name):
+# write config data 
+class edit_config(read_in):
+    def __init__(self):
         super().__init__
-        self.f = open(self.name,'a+')
+
+    def find_match(self, **change):
+        self.change = change
+        # can be opt algorithm
+        for line in self.list:
+            for item in self.change:
+                if "=" in line:
+                    check = line.strip().split(" = ")
+                    if check[0] == 'include':
+                        pass
+                    else:
+                        pass
+
+    def comment_out(self):
+        pass
 
 # create new config file
-class create_new:
-    def __init__(self, name, N = 0):
+class create_new(read_in):
+    def __init__(self, N = 0):
+        super().__init__
         etype = ("periodic", "event-driven")
-        self.name = name
         self.type = etype[N]
     def new(self):
-        periotem = Path.cwd() / f"server.{self.type}.conf"
+        template = Path.cwd() / f"server/server.{self.type}.conf"
         self.confname = f"server.{self.name}.conf"
-        shutil.copyfile(periotem, configpath + self.confname)
-        if self.type == "event-driven":
-            pass
-        else:
-            pass
+        newpath = configpath + self.confname
+        with open(newpath, 'w+') as fw:
+            logging.debug("write mode w+")
 
 
-    
 
 
+
+# fw.write(f"# configuration for {self.name}"+"\n")
 
 if __name__ == "__main__": # for testing
-    pass
-
+    test = create_new("ABC")
+    test.new()
 
 
 
